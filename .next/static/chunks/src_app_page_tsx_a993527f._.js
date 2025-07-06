2327818deb3a80803af62fe7a2e55fb559dcd546
nataliaccusi <lucero.cat.9@hotmail.com>
2025-07-06 02:03:37 -0500

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751785416899

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_28975bb2._.js",
+  "static/chunks/src_7ed08878._.js"
+],
+    source: "dynamic"
+});
