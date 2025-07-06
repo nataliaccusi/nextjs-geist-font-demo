8bacfeb0dab5540e2aeab3766569954b3c930bff
nataliaccusi <lucero.cat.9@hotmail.com>
2025-07-06 02:03:48 -0500

    Add .next/static/chunks/src_app_lawyer_[id]_page_tsx_a993527f._.js 1751785427763

---
+++ .next/static/chunks/src_app_lawyer_[id]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_e167d76b._.js",
+  "static/chunks/node_modules_ca96bffb._.js"
+],
+    source: "dynamic"
+});
