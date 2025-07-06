8dc10cd45883d808b1865b57980a963958cd32a8
nataliaccusi <lucero.cat.9@hotmail.com>
2025-07-06 02:03:31 -0500

    Add .next/static/chunks/src_app_chat_page_tsx_a993527f._.js 1751785410428

---
+++ .next/static/chunks/src_app_chat_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e44660b7._.js",
+  "static/chunks/src_df8ea7c1._.js"
+],
+    source: "dynamic"
+});
